ca7f7aa4ac9dfe22cc69a71028fbe6dadc5ebd1b
mzajac <matzaj101@gmail.com>
2019-01-08 22:17:57 +0100

    first layout

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,9 @@
+export class Ingresient {
+    // name: string;
+    // amount: number; zamiast tych prop uzyc public w ctor
+
+    constructor(public name: string, public amount: number) {
+        // this.name = name;
+        // this.amount = amount;
+    }
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingresient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingresient[] = [
+    new Ingresient('apples', 5),
+    new Ingresient('tomatoes ', 10  )
+  ];
   constructor() { }
 
   ngOnInit() {
